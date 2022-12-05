0edeb333fbffa63a27065009be7580afdec3f132
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-12-05 18:38:49 +0700

    header and banner

--- src/customer/page/home/banner/banner.js
+++ src/customer/page/home/banner/banner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'antd';
 import clsx from 'clsx';
 import style from "./banner.module.scss";
@@ -8,6 +8,13 @@ function Banner() {
     const onChange = (currentSlide) => {
         console.log(currentSlide);
     };
+    //     const ani = (time) => {
+    //         return (
+    //             transform: "translateY('0')"
+    //        )
+    // }
+    const [ic, setIc] = useState(0)
+    console.log(ic);
     const banner = [
         "https://ninetheme.com/themes/styler/fashion/wp-content/uploads/2022/02/black-gingham-belted-mini-shirt-dress-e1639323379980.jpeg",
         "https://ninetheme.com/themes/styler/fashion/wp-content/uploads/2021/12/product-name-2.jpeg",
@@ -17,15 +24,15 @@ function Banner() {
     // const im = ''
     return (<>
         <div className={clsx(style.banner)}>
-            <Carousel dots={true} autoplay afterChange={onChange}>
+            <Carousel dots={true} autoplay beforeChange={(p, n) => setIc(n)}>
                 {banner.map((item, index) => {
                     return (
-                        <div>
-                            <div style={{ backgroundImage: `url(${item})` }} className={clsx(style.item)}>
+                        <div key={index}>
+                            <div style={{ backgroundImage: `url(${item})`, }} className={clsx(style.item)}>
                                 <div className={clsx(style.content, index === 0 ? style.left : style.right)}>
-                                    <span className={style.title}>Winter collection #2022</span>
-                                    <h1 className={style.name}>Modern Pastel Colors</h1>
-                                    <Link to="/shop" className={clsx(style.btn_shop)}><span>Shop Now <i class="fa-solid fa-chevron-right"></i></span> </Link>
+                                    <span className={clsx(style.title, ic === index && style.ani)}>Winter collection #2022</span>
+                                    <h1 className={clsx(style.name, ic === index && style.ani)}>Modern Pastel Colors</h1>
+                                    <Link to="/shop" className={clsx(style.btn_shop, ic === index && style.ani)}><span>Shop Now <i className="fa-solid fa-chevron-right"></i></span> </Link>
                                 </div>
                             </div>
                         </div>
